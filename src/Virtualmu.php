c03fb2f61aae9a473bb4c20f1c2ed8d9060ae7d2
vpaymu <alchemy.2608@gmail.com>
2024-04-14 20:38:47 +0700

    ok

--- src/Virtualmu.php
+++ src/Virtualmu.php
@@ -5,54 +5,88 @@ namespace Virtualdev\Virtualapi;
 class Virtualmu
 {
     private string $endpoint;
+
     public function __construct(string $endpoint)
     {
         $this->endpoint = $endpoint;
     }
+
     private function sendGetRequest(string $url): array
     {
         $ch = curl_init($url);
-
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPGET, true);
-
         $response = curl_exec($ch);
-        $error = curl_error($ch);
-
         curl_close($ch);
-
-        if ($error) {
-            throw new \Exception($error);
-        }
-
         return json_decode($response, true);
+    }
+
+    private function buildUrl(string $path, array $params = []): string
+    {
+        $url = rtrim($this->endpoint, '/') . '/' . ltrim($path, '/');
+        if (!empty($params)) {
+            $url .= '?' . http_build_query($params);
+        }
+        return $url;
+    }
+
+    private function getRequest(string $path, array $params = []): array
+    {
+        $url = $this->buildUrl($path, $params);
+        return $this->sendGetRequest($url);
     }
 
     public function cekDataGame(): array
     {
-        $url = "{$this->endpoint}/";
-
-        return $this->sendGetRequest($url);
+        return $this->getRequest('/');
     }
 
-    public function cekDataBank(): array
+    public function cekDataGameServer(string $slug): array
     {
-        $url = "{$this->endpoint}/rekening";
+        return $this->getRequest("/api/game/get-zone/$slug");
+    }
 
-        return $this->sendGetRequest($url);
+    public function cekDataRekening(): array
+    {
+        return $this->getRequest('/rekening');
+    }
+
+    public function cekKodeRekening(string $slug): array
+    {
+        return $this->getRequest("/api/rekening/get-kode/$slug");
     }
 
     public function cekDataPln(): array
     {
-        $url = "{$this->endpoint}/pln";
-
-        return $this->sendGetRequest($url);
+        return $this->getRequest('/pln');
     }
 
     public function cekDataPdam(): array
     {
-        $url = "{$this->endpoint}/pdam";
+        return $this->getRequest('/pdam');
+    }
 
-        return $this->sendGetRequest($url);
+    public function cekIdGame(string $slug, string $id): array
+    {
+        return $this->getRequest("/api/game/$slug", ['id' => $id]);
+    }
+
+    public function cekIdGameServer(string $slug, string $id, string $server): array
+    {
+        return $this->getRequest("/api/game/$slug", ['id' => $id, 'zone' => $server]);
+    }
+
+    public function cekIdRekening(string $slug, string $tujuan, string $kode): array
+    {
+        return $this->getRequest("/api/rekening/$slug", ['tujuan' => $tujuan, 'kode' => $kode]);
+    }
+
+    public function cekIdPln(string $slug, string $id): array
+    {
+        return $this->getRequest("/api/pln/$slug", ['id' => $id]);
+    }
+
+    public function cekIdPdam(string $slug, string $id): array
+    {
+        return $this->getRequest("/api/pdam/$slug", ['id' => $id]);
     }
 }
